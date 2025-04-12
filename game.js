0904b921a0fab3dccf6c55f1c6eae796f3a29504
Peter Nguyen <nguye5he@mail.uc.edu>
2025-04-11 21:24:56 -0400

    added roles

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 //Assignment: Deep Dive Dash Final Project
-// Contributors:  Addison Hatfield (Lead Game Developer), Bella Orr (Game Developer/ Tester), Nicole Miller (Lead Game Designer), Peter Nguyen 
+// Contributors:  Addison Hatfield (Lead Game Developer), Bella Orr (Game Developer/ Tester), Nicole Miller (Lead Game Designer), Peter Nguyen (Game Designer, UXUI, Documentation)
 // (Game Designer/Documentation)
 // Course: IT3049C
 // Professor: Professor Andrew Lively
